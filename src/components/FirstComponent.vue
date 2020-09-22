623ceb5472bb67b7360c797cba197d308bce7580
William Araujo <williamf1f2@gmail.com>
2020-09-22 19:21:58 -0300

    Testa reatividade com v-model em input

--- src/components/FirstComponent.vue
+++ src/components/FirstComponent.vue
@@ -1,36 +1,52 @@
 <template>
-  <div>
-    <div class="container animate__animated animate__fadeIn">
+    <div class="container animate__animated animate__fadeIn animate__fast">
       <h1 class="title">{{ title }}</h1>
-
-      <div class="text-wrapper">
+      <!-- <div class="text-wrapper">
         <span class="highlight-text">Vue.js is really awesome!</span>
+      </div> -->
+      <div>
+        <!-- <InputText placeholder="type something nice" v-model="inputValue"/> -->
+        <input
+          class="text-input"
+          type="text"
+          placeholder="type something nice"
+          v-model="inputValue"
+        />
       </div>
-
-      <button class="btn-click" v-on:click="displayMessage">{{ textButton }}</button>
-      <div class="container animate__animated animate__fadeInDown animate__fast" v-if="showMessage">
-        <span>XPTO</span>
+      <button class="btn-send" v-on:click="displayMessage">SEND</button>
+      <button class="btn-delete" v-on:click="hideMessage">DELETE</button>
+      <div class="show-text-container">
+        <span>{{ displayedValue }}</span>
       </div>
     </div>
-  </div>
 </template>
 
 <script>
+// import InputText from "./InputText";
+
 export default {
   name: "FirstComponent",
+  components: {
+    // InputText
+  },
   data() {
     // console.log("props:", this.$props);
     return {
       showMessage: false,
       textButton: "SHOW",
+      inputValue: "",
+      displayedValue: "",
     };
   },
   methods: {
-    displayMessage() {
-      this.showMessage = !this.showMessage;
-      this.textButton = this.textButton === "SHOW" ? "HIDE" : "SHOW";
-      console.log("botão clicado", this.showMessage);
+    displayMessage: function () {
+      this.showMessage = true;
+      this.displayedValue = this.inputValue.trim() === "" ? "no value sent" : this.inputValue;
     },
+    hideMessage: function () {
+      this.showMessage = false;
+      this.inputValue = "";
+    }
   },
   props: {
     title: String,
@@ -41,10 +57,19 @@ export default {
 <style scoped>
 .container {
   background-color: rgb(255, 255, 255, 0.1);
-  margin: 2rem;
+  margin: 25px 15px 0 15px;
   padding: 3rem;
   border-radius: 0.4rem;
   border: 2px solid #4abf8a;
+}
+
+.show-text-container {
+  background-color: rgb(255, 255, 255, 0.1);
+  margin-top: 25px;
+  padding: 3rem;
+  border-radius: 0.4rem;
+  border: 2px solid #4abf8a;
+  height: 126px;
 }
 
 .highlight-text {
@@ -53,7 +78,7 @@ export default {
 }
 
 .text-wrapper {
-  margin: 2rem 0;
+  margin-top: 2rem;
 }
 
 .title {
@@ -66,7 +91,7 @@ span {
   color: white;
 }
 
-.btn-click {
+.btn-send {
   width: 180px;
   background-color: #4abf8a;
   padding: 1rem 2rem;
@@ -74,10 +99,40 @@ span {
   font-weight: bold;
   font-size: 1.2rem;
   color: white;
+  margin-right: 30px;
 }
-
-.btn-click:hover {
+.btn-send:hover {
   background-color: #368863;
   cursor: pointer;
+}
+.btn-send:focus {
+  background-color: #368863;
+}
+
+.btn-delete {
+  width: 180px;
+  background-color: #b94848;
+  padding: 1rem 2rem;
+  border-radius: 0.4rem;
+  font-weight: bold;
+  font-size: 1.2rem;
+  color: white;
+  margin-left: 30px;
+}
+.btn-delete:hover {
+  background-color: #883636;
+  cursor: pointer;
+}
+.btn-delete:focus {
+  background-color: #883636;
+}
+
+.text-input {
+  width: 100%;
+  padding: 0.7rem;
+  font-size: 1.2rem;
+  margin: 1rem 0;
+  border-bottom: 2px solid #4abf8a;
+  color: white;
 }
 </style>
---
+++ src/components/InputText.vue
@@ -0,0 +1,23 @@
+<template>
+  <input class="text-input" type="text" :placeholder="placeholder" />
+</template>
+
+<script>
+export default {
+  name: "InputText",
+  props: {
+    placeholder: String,
+  }
+};
+</script>
+
+<style scoped>
+.text-input {
+  width: 100%;
+  padding: .7rem;
+  font-size: 1rem;
+  margin: 1rem 0;
+  border-bottom: 2px solid #4abf8a;
+  color: white;
+}
+</style>
